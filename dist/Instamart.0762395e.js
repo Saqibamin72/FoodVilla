1899614559aab6706177f55e64d51d555c841a63
Saqib <Saqibamin72@gmail.com>
2024-05-30 14:51:21 +0530

    accordian added

--- dist/Instamart.0762395e.js
+++ dist/Instamart.0762395e.js
@@ -614,40 +614,108 @@ try {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 parcelHelpers.defineInteropFlag(exports);
 var _jsxDevRuntime = require("react/jsx-dev-runtime");
-const Instamart = ()=>{
+var _react = require("react");
+var _s = $RefreshSig$();
+const Section = ({ title, description })=>{
+    _s();
+    const [isVisible, setIsVisisble] = (0, _react.useState)(false);
     return /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("div", {
+        className: "border border-black p-2 m-0",
         children: [
-            /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("h1", {
-                children: "Instamart"
+            /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("h3", {
+                className: "font-bold text-xl",
+                children: title
             }, void 0, false, {
                 fileName: "src/components/Instamart.js",
-                lineNumber: 3,
-                columnNumber: 9
+                lineNumber: 8,
+                columnNumber: 13
             }, undefined),
-            /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("h1", {
-                children: "100 components inside it"
+            isVisible ? /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("button", {
+                onClick: ()=>setIsVisisble(false),
+                className: "cursor-pointer underline",
+                children: "Hide"
             }, void 0, false, {
                 fileName: "src/components/Instamart.js",
-                lineNumber: 4,
-                columnNumber: 9
+                lineNumber: 10,
+                columnNumber: 26
+            }, undefined) : /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("button", {
+                onClick: ()=>setIsVisisble(true),
+                className: "cursor-pointer underline",
+                children: "Show"
+            }, void 0, false, {
+                fileName: "src/components/Instamart.js",
+                lineNumber: 11,
+                columnNumber: 31
+            }, undefined),
+            isVisible && /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("p", {
+                children: description
+            }, void 0, false, {
+                fileName: "src/components/Instamart.js",
+                lineNumber: 14,
+                columnNumber: 27
             }, undefined)
         ]
     }, void 0, true, {
         fileName: "src/components/Instamart.js",
-        lineNumber: 2,
+        lineNumber: 7,
         columnNumber: 10
     }, undefined);
 };
-_c = Instamart;
+_s(Section, "SkNMisw7BBTRdcyaNs0sUMUtI2c=");
+_c = Section;
+const Instamart = ()=>{
+    return /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("div", {
+        children: [
+            /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("h1", {
+                className: "text-3xl font-bold m-2 p-2",
+                children: "Instamart"
+            }, void 0, false, {
+                fileName: "src/components/Instamart.js",
+                lineNumber: 19,
+                columnNumber: 13
+            }, undefined),
+            /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)(Section, {
+                title: "About Instamart",
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            }, void 0, false, {
+                fileName: "src/components/Instamart.js",
+                lineNumber: 20,
+                columnNumber: 13
+            }, undefined),
+            /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)(Section, {
+                title: "Team Instamart",
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            }, void 0, false, {
+                fileName: "src/components/Instamart.js",
+                lineNumber: 21,
+                columnNumber: 14
+            }, undefined),
+            /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)(Section, {
+                title: "Careers",
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            }, void 0, false, {
+                fileName: "src/components/Instamart.js",
+                lineNumber: 22,
+                columnNumber: 16
+            }, undefined)
+        ]
+    }, void 0, true, {
+        fileName: "src/components/Instamart.js",
+        lineNumber: 18,
+        columnNumber: 10
+    }, undefined);
+};
+_c1 = Instamart;
 exports.default = Instamart;
-var _c;
-$RefreshReg$(_c, "Instamart");
+var _c, _c1;
+$RefreshReg$(_c, "Section");
+$RefreshReg$(_c1, "Instamart");
 
   $parcel$ReactRefreshHelpers$8238.postlude(module);
 } finally {
   window.$RefreshReg$ = prevRefreshReg;
   window.$RefreshSig$ = prevRefreshSig;
 }
-},{"react/jsx-dev-runtime":"iTorj","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3","@parcel/transformer-react-refresh-wrap/lib/helpers/helpers.js":"km3Ru"}]},["5u83V","1xC6H"], null, "parcelRequire7f02")
+},{"react/jsx-dev-runtime":"iTorj","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3","@parcel/transformer-react-refresh-wrap/lib/helpers/helpers.js":"km3Ru","react":"21dqq"}]},["5u83V","1xC6H"], null, "parcelRequire7f02")
 
 //# sourceMappingURL=Instamart.0762395e.js.map
--- src/components/Instamart.js
+++ src/components/Instamart.js
@@ -1,10 +1,40 @@
-const Instamart=()=>{
-    return(
-    <div>
-        <h1>Instamart</h1>
-        <h1>100 components inside it</h1>
+import { useState } from "react"
 
-    </div>
+const Section = ({ title,description }) => {
+    const [isVisible, setIsVisisble]= useState(false);
+    return (
+        <div className="border border-black p-2 m-0">
+            <h3 className="font-bold text-xl">{title}</h3>
+            
+            {
+              isVisible ? (<button  onClick={()=>setIsVisisble(false)}className="cursor-pointer underline">
+              Hide</button>):
+                (<button  onClick={()=>setIsVisisble(true)}className="cursor-pointer underline">
+                Show</button>)
+            }
+                
+            {isVisible && <p>{description}</p>}
+        </div>
+    )
+}
+const Instamart = () => {
+
+    return (
+        <div>
+            <h1 className="text-3xl font-bold m-2 p-2">Instamart</h1>
+            <Section 
+            title={"About Instamart"}
+            description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
+            />
+             <Section 
+            title={"Team Instamart"}
+            description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
+            />
+               <Section 
+            title={"Careers"}
+            description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
+            />
+        </div>
     )
 }
 export default Instamart;
